a56e878a5b8856d16f5f9b69bf8a7c40012b7cde
Fabian Lenz <lenz_fabian@yahoo.de>
2018-02-27 16:56:51 +0100

    Add project files.

---
+++ BindingModeTest/FunFunFun2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace BindingModeTest
+{
+    public class FunFunFun2 : TextBox
+    {
+        public static readonly DependencyProperty ValueDecimalProperty
+          = DependencyProperty.RegisterAttached(nameof(ValueDecimal), typeof(decimal), typeof(FunFunFun2),
+               new FrameworkPropertyMetadata(new PropertyChangedCallback(On_ValueDecimal_Changed))
+               {
+                   BindsTwoWayByDefault = true,
+                   DefaultUpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+               });
+
+        private static void On_ValueDecimal_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            FunFunFun2 maskedNumericInput = (FunFunFun2)d;
+            maskedNumericInput.ValueDecimal = Convert.ToDecimal(e.NewValue);
+        }
+
+        public decimal? ValueDecimal
+        {
+            get
+            {
+                if (base.Text == null)
+                    return null;
+
+                return Convert.ToDecimal(GetValue(ValueDecimalProperty));
+            }
+            set
+            {
+                SetValue(ValueDecimalProperty, value);
+                base.Text = value.ToString();
+            }
+        }
+
+
+        public void InitBinding(string propertyName)
+        {
+            var binding = new Binding(propertyName) { UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged, Mode = BindingMode.OneWay };
+            SetBinding(ValueDecimalProperty, binding);
+        }
+    }
+}
---
+++ BindingModeTest/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BindingModeTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        decimal input;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public decimal Input
+        {
+            get
+            {
+                return input;
+            }
+
+            set
+            {
+                input = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Input)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Output)));
+            }
+        }
+
+        public decimal Output => Input;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+            funBox.InitBinding(nameof(Output));
+        }
+    }
+}
